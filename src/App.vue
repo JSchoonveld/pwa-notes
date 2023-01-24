a022baac1cc800c51345eed0c378646fa056e728
Jesse Schoonveld <jesse.schoonveld@beeproger.com>
2023-01-24 16:54:02 +0100

    init project, added Tiptap and Tailwind

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import Tiptap from "./components/Tiptap.vue";
+</script>
+
+<template>
+  <div class="flex w-screen h-screen text-gray-700">
+    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
+
+    </div>
+    <div class="flex flex-col flex-grow">
+      <div class="flex flex-col flex-grow overflow-auto prose lg:prose-xl">
+        <editor-content :editor="editor"></editor-content>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+
+export default {
+  name: 'App',
+  components: {
+    EditorContent
+  },
+  data() {
+    return {
+      editor: null
+    }
+  },
+  mounted() {
+    this.editor = new Editor({
+      content: '',
+      extensions: [
+          StarterKit
+      ]
+    })
+  },
+  beforeUnmount() {
+    this.editor.destroy();
+  }
+}
+</script>
+
+<style>
+h1 {
+  font-size: 24px;
+  font-weight: bold;
+  color: red;
+}
+
+h2 {
+  font-size: 20px;
+  color: blue;
+}
+</style>
